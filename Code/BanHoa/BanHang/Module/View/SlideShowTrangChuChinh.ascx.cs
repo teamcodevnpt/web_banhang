3b8c8c2370c509a10f884829bf04a94d81d6d39f
DESKTOP-43VMKLC\nam01 <nam01678@gmail.com>
2017-02-21 19:49:21 +0700

    Thêm CKEditor

--- Code/BanHoa/BanHang/Module/View/SlideShowTrangChuChinh.ascx.cs
+++ Code/BanHoa/BanHang/Module/View/SlideShowTrangChuChinh.ascx.cs
@@ -9,6 +9,18 @@ public partial class Module_View_SlideShowTrangChuChinh : System.Web.UI.UserCont
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        load_noidung();
+    }
+    void load_noidung()
+    {
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                ltr_noidung.Text += "<li>" +
+                    "<img  style='height:400px; width=100%' class='img-responsive'  src='" + ResolveUrl("~/img/" + i + ".jpg") + "'/>" +
+                    " <p class='caption'>BAN HOA</p></li>";
+            }
 
+        }
     }
 }
--- Code/BanHoa/BanHang/QuanTri/Default.aspx
+++ Code/BanHoa/BanHang/QuanTri/Default.aspx
@@ -1,3 +1,11 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="QuanTri_Default" MasterPageFile="~/QuanTri/QuanTri.master" %>
+<%@ Register Assembly="CKEditor.NET" Namespace="CKEditor.NET" TagPrefix="CKEditor" %>
+<html>
+    <head>
+    </head>
+    <body>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentTrangChu" runat="server">
 </asp:Content>
+        
+    </body>
+</html>
